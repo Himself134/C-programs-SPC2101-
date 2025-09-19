1d39995613d3c23a438ec4fa84026167a5f9a536
Himself134 <edwardoyier38@gmail.com>
2025-09-19 13:35:32 +0300

    Add files via upload

---
+++ UNITS DISPLAY.c
@@ -0,0 +1,18 @@
+//Software engineering units 
+/*
+Name:Edward Hamillton 
+Reg No:PA106/G/28816/25
+Description:Display of the units for the semester
+*/
+#include <stdio.h>
+int main(){
+	printf("UCU 2106 Fundamentals of ict\n");
+	printf("SPM 2104 Mathematics for science\n");
+	printf("SPI 2102 Computer systems and organizations\n");
+	printf("UCU 2106 Communication and information literacy skills\n");
+	printf("SPM 2100 Discrete mathematics\n");
+	printf("SPC 2101 Introduction to programming and problem solving\n");
+	printf("SSE 2101 Fundamentals of system analysis and design\n");
+	
+	return 0;
+}
